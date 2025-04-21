54c48498157180c79122df179e7d977e4c6cc7ca
ryanrmills <ryanroymills@gmail.com>
2025-04-21 15:06:23 -0700

    added utils to CreditCard

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,27 @@
 public class CreditCard {
-    
+    private String name;
+    private double balance;
+    private double apr;
+
+    public CreditCard(String name, double balance, double apr){
+        this.name = name;
+        this.balance = balance;
+        this.apr = apr;
+    }
+
+    public double getApr(){
+        return apr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String toString(){
+        return name + ": " + balance;
+    }
 }
