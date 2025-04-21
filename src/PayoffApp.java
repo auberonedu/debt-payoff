96ed5aa81d5a34b9983c9326426c5096091e9c40
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-04-21 11:46:17 -0700

    Added formating to check for credit card and balances

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -5,29 +5,29 @@ import java.util.List;
 
 public class PayoffApp {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    
 
-        List<Double> balances = new ArrayList<Double>();
+         Scanner scan = new Scanner(System.in);
+
+         List<Double> balances = new ArrayList<Double>();
 
 
         while(scan.hasNextLine()) {
-            String name = scan.nextLine();
+             String name = scan.nextLine();
 
-            double apr = scan.nextDouble();
+             double apr = scan.nextDouble();
             double balance = scan.nextDouble();
 
-            // Consume \n after balance input 
+             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
 
             balances.add(balance);
 
-            String aprString = String.format("%.2f%%", apr);
-            String balanceString = String.format("$%.2f", balance);
-            System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
-        }
+             String aprString = String.format("%.2f%%", apr);
+             String balanceString = String.format("$%.2f", balance);
+             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+         }
 
-        System.out.println(balances);
-        Collections.sort(balances,Collections.reverseOrder());
-        System.out.println(balances);  
+
     }
 }
