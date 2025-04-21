6d495cdf2111b32fe801a8080ba6c41711a9f290
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-04-21 11:42:32 -0700

    Added setters and toStrings

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,26 @@
 public class CreditCard {
     
+    private String name;
+    private double apr;
+    private double balance;
+    
+    public CreditCard(String name, double apr, double balance){
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance;
+    }
+    public double getApr(){
+        return apr;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    @Override
+
+    public String toString(){
+        return "Card Name: " + name + ", Balance: "+ balance + ", APR: "+ apr +" %"; 
+    }
 }
