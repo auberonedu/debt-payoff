0bfd7e9a5dfe796510c455ac8ae55a979ce7a814
konradkelly <konradky@gmail.com>
2025-04-21 11:44:10 -0700

    Added CreditCard class setters and getters

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,28 @@
 public class CreditCard {
+    private String name;
+    private double apr;
+    private double balance;
+
+    public CreditCard(String name, double apr, double balance) {
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance;
+    }
     
+    public double getApr() {
+        return apr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " " + balance;
+    }
 }
