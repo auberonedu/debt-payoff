2371fc426e50882d485d9fa45b35de2dbfcbcdd8
Brendan Villaraza <villaraza.brendan@student.greenriver.edu>
2025-04-21 11:43:09 -0700

    Added credit cards, and to string

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,28 @@
 public class CreditCard {
+    private String name;
+    private double apr;
+    private double balance;
+
+    public CreditCard(String name, double apr, double balance){
+        this.name = name;
+        this.apr = apr;
+        this.balance = balance;
+    }
     
+    public double getApr(){
+        return apr;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + balance;
+    }
 }
