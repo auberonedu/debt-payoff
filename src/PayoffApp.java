dc55c88e9a96239da90b3c649052f9097e04c3e2
konradkelly <konradky@gmail.com>
2025-04-21 13:40:53 -0700

    displayed all CreditCards in an ArrayList

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -7,9 +7,20 @@ import java.util.*;
 
 public class PayoffApp {
     public static void main(String[] args) {
+        CreditCard amex = new CreditCard("American Express Platinum", 25, 1300);
+        System.out.println(amex.getApr());
+
+        CreditCard amexBasic = new CreditCard("American Express Basic", 33, 200);
+        System.out.println(amexBasic.getApr());
+
+        CreditCard chase = new CreditCard("Chase Sapphire", 21, 321);
+        System.out.println(chase.getApr());
+        System.out.println(chase);
+
+        System.out.println(amex.getBalance());
         Scanner scan = new Scanner(System.in);
 
-        List<Double> balances = new ArrayList<Double>();
+        List<CreditCard> cards = new ArrayList<CreditCard>();
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -20,18 +31,25 @@ public class PayoffApp {
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
 
-            balances.add(balance);
+            CreditCard myCard = new CreditCard(name, apr, balance);
+            
+            cards.add(myCard);
 
-            String aprString = String.format("%.2f%%", apr);
-            String balanceString = String.format("$%.2f", balance);
-            System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+            System.out.println(myCard);
+
+
+            //String aprString = String.format("%.2f%%", apr);
+            //String balanceString = String.format("$%.2f", balance);
+            //System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+       // }
+        //System.out.println(cards);
+
+       // Collections.sort(cards);
+        //System.out.println(cards);
+
+       // Collections.sort(cards, Collections.reverseOrder());
+        //System.out.println(cards);
+
         }
-        System.out.println(balances);
-
-        Collections.sort(balances);
-        System.out.println(balances);
-        
-        Collections.sort(balances, Collections.reverseOrder());
-        System.out.println(balances);
     }
 }
