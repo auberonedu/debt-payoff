c35dc1a0eee4d159da7d067edbd5522e39c2054c
Masih <masihrahmani87@gmail.com>
2025-04-21 11:42:51 -0700

    Added class to CreditCard

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,3 +1,30 @@
 public class CreditCard {
     
+  private String name;
+  private double Apr;
+  private double balance;
+
+  public CreditCard(String name , double Apr, double balance ){
+    this.name = name;
+    this.Apr = Apr;
+    this.balance = balance;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public double getApr(){
+    return Apr; 
+  }
+
+  @Override
+  public String toString(){
+    return name + " "  + balance;
+  }
+
 }
