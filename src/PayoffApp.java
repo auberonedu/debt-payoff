6513a44fdff805fcc914d89d9d67551ffb72489f
konradkelly <konradky@gmail.com>
2025-04-23 11:15:58 -0700

    compareTo method for monthly interest

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -7,15 +7,22 @@ import java.util.*;
 
 public class PayoffApp {
     public static void main(String[] args) {
-        CreditCard amex = new CreditCard("American Express Platinum", 25, 1300);
+        CreditCard amex = new CreditCard("American Express Platinum", 22, 1007);
         System.out.println(amex.getApr());
 
         CreditCard amexBasic = new CreditCard("American Express Basic", 33, 200);
         System.out.println(amexBasic.getApr());
 
-        CreditCard chase = new CreditCard("Chase Sapphire", 21, 321);
-        System.out.println(chase.getApr());
-        System.out.println(chase);
+        CreditCard discover = new CreditCard("Discover It", 33, 500);
+        
+        System.out.println(amex.compareTo(discover));
+        System.out.println(discover.compareTo(amex));
+        System.out.println(amex.monthlyInterest());
+        System.out.println(discover.monthlyInterest());
+
+        //CreditCard chase = new CreditCard("Chase Sapphire", 21, 321);
+        //System.out.println(chase.getApr());
+        //System.out.println(chase);
 
         System.out.println(amex.getBalance());
         Scanner scan = new Scanner(System.in);
@@ -38,18 +45,18 @@ public class PayoffApp {
             System.out.println(myCard);
 
 
-            //String aprString = String.format("%.2f%%", apr);
-            //String balanceString = String.format("$%.2f", balance);
-            //System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+            String aprString = String.format("%.2f%%", apr);
+            String balanceString = String.format("$%.2f", balance);
+            System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
        // }
-        //System.out.println(cards);
-
-       // Collections.sort(cards);
-        //System.out.println(cards);
-
-       // Collections.sort(cards, Collections.reverseOrder());
-        //System.out.println(cards);
-
+        System.out.println(cards);
+        Collections.sort(cards);
+        
+        System.out.println(cards);
+        Collections.sort(cards, Collections.reverseOrder());
+        System.out.println(cards);
+        Collections.sort(cards);
+        System.out.println(cards);
         }
     }
 }
