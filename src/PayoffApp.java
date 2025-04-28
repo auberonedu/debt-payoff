92203d26880d6c5282ba3a4cb89e3465535a5e25
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-04-28 11:26:34 -0700

    Added card map

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,18 +1,47 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
-        CreditCard amex = new CreditCard("American Ezp", 22, 1007);
-        CreditCard discover = new CreditCard("Discover It", 33, 500);
+        //Map<String, Integer> schoolAges = new HashMap<>();
 
-        System.out.println(amex.monthlyInterest());
+        // schoolAges.put("GRC", 40);
+        // schoolAges.put("WSU", 100);
+        // schoolAges.put("BC",35);
+        // schoolAges.put("WSU", 135);
+
+        //Print out age of WSU
+        // System.out.println(schoolAges.get("WSU"));
+
+        // for(var entry : schoolAges.entrySet()){
+        //     System.out.println(entry.getKey() + " : " + entry.getValue());
+        // }
+
+
+        // for(Map.Entry<String , Integer> entry : schoolAges.entrySet()){
+        //     System.out.println(entry.getKey() + " : " + entry.getValue());
+        // }
+
+        // CreditCard amex = new CreditCard("American Ezp", 22, 1007);
+        // CreditCard discover = new CreditCard("Discover It", 33, 500);
+
+        // Map<String, CreditCard> cardMap = new HashMap<>();
+        // cardMap.put("American Express", amex);
+        // cardMap.put("Discover It", discover);
+
+        // CreditCard myCard = cardMap.get("American Express");
+        // System.out.println(cardMap.get("American Express"));
+
+        // System.out.println(amex.monthlyInterest());
 
         Scanner scan = new Scanner(System.in);
 
         List<CreditCard> cards = new ArrayList();
+
+        Map<String, CreditCard> carMap = new HashMap<>();
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
@@ -27,14 +56,18 @@ public class PayoffApp {
 
             cards.add(mycard);
 
+            carMap.put(name, mycard);
+
             // String aprString = String.format("%.2f%%", apr);
             // String balanceString = String.format("$%.2f", balance);
             // System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
         }
 
         
-        Collections.sort(cards, Collections.reverseOrder());
-        System.out.println(cards);
+        // Collections.sort(cards, Collections.reverseOrder());
+        // System.out.println(cards);
+
+        System.out.println(carMap);
        
     }
 }
