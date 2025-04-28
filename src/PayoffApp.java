c373d810215b28f76f6535fdcafd9b33bd4ca17f
Kelley Castillo <kelleycastillo@Kelleys-MacBook-Pro.local>
2025-04-28 11:26:55 -0700

    added hashmap

--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,43 +1,76 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class PayoffApp {
     public static void main(String[] args) {
-        CreditCard amex = new CreditCard("American Expresss", 22, 1007);
-        CreditCard discover = new CreditCard("Discover It", 33, 500);
+        // Map<String, Integer> schoolAges = new HashMap<>();
+        // schoolAges.put("GRC",40);
+        // schoolAges.put("WSU",100);
+        // schoolAges.put("BC",35);
+        // schoolAges.put("WSU",135);
 
-       System.out.println(amex.monthlyInterest());
-    }
-}
+        //print out age of WSU
+        // System.out.println(schoolAges.get("WSU"));
+
+
+        //uses 'var' instead of (Map.Entry<String,Integer>
+        // for(var entry : schoolAges.entrySet()) {
+        //     System.out.println(entry.getKey() + ":" + entry.getValue());
+        // }
+
+        // for(Map.Entry<String,Integer> entry : schoolAges.entrySet()) {
+        //     System.out.println(entry.getKey() + ":" + entry.getValue());
+        // }
+
+
+        // CreditCard amex = new CreditCard("American Expresss", 22, 1007);
+        // CreditCard discover = new CreditCard("Discover It", 33, 500);
+
+        // Map<String, CreditCard> cardMap = new HashMap<>();
+        // cardMap.put("American Express", amex);
+        // cardMap.put("Discover It", discover);
+
+        // System.out.println(cardMap.get("American Express"));
+
+        // CreditCard myCard = cardMap.get("American Express");
+        // System.out.println(myCard.getApr());
+
+    //    System.out.println(amex.monthlyInterest());
        
-//         Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-//        List<CreditCard> cards = new ArrayList<CreditCard>();
+       List<CreditCard> cards = new ArrayList<CreditCard>();
+       Map<String, CreditCard> cardMap = new HashMap<>();
 
-//         while(scan.hasNextLine()) {
-//             String name = scan.nextLine();
+        while(scan.hasNextLine()) {
+            String name = scan.nextLine();
 
-//             double apr = scan.nextDouble();
-//             double balance = scan.nextDouble();
+            double apr = scan.nextDouble();
+            double balance = scan.nextDouble();
 
-//             // Consume \n after balance input 
-//             if(scan.hasNextLine()) scan.nextLine();
+            // Consume \n after balance input 
+            if(scan.hasNextLine()) scan.nextLine();
 
-//             CreditCard myCard = new CreditCard(name, apr, balance);
+            CreditCard myCard = new CreditCard(name, apr, balance);
             
-//             cards.add(myCard);
-
-
-//             String aprString = String.format("%.2f%%", apr);
-//             String balanceString = String.format("$%.2f", balance);
-//             System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
-//         }
-
-//         System.out.println(cards);
-//         Collections.sort(cards, Collections.reverseOrder());
-//         System.out.println(cards);
+            cards.add(myCard);
+            cardMap.put(name, myCard);
         
-//     }
-// }
+        //     String aprString = String.format("%.2f%%", apr);
+        //     String balanceString = String.format("$%.2f", balance);
+        //     System.out.println(name + ": " + "APR: " + aprString + " Balance: " + balanceString);
+        // 
+        }
+
+        // System.out.println(cards);
+        // Collections.sort(cards, Collections.reverseOrder());
+        // System.out.println(cards);
+        System.out.println(cardMap);
+        
+ }
+}
+
